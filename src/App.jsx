c2310072b7a60e6572047558a0cc7965bd40507c
sachinchaves <sachin.chaves1991@gmail.com>
2025-06-28 22:19:12 -0700

    Add initial structure and add gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="x" />
+          <Player name="Player 2" symbol="0" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
