9d462165dc1be187c206517be9850ac8a1e84e3d
sachinchaves <sachin.chaves1991@gmail.com>
2025-06-28 22:55:51 -0700

    Update player name as per data the user inputs

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="x" />
-          <Player name="Player 2" symbol="0" />
+          <Player initialName="Player 1" symbol="x" />
+          <Player initialName="Player 2" symbol="0" />
         </ol>
         GAME BOARD
       </div>
